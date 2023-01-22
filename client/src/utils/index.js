6508630c2d5103728395c6019026c231b89b9fdf
Kage <romzhey@gmail.com>
2023-01-22 23:17:30 +0800

    back-end and front end integration

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,9 +1,14 @@
 import { surpriseMePrompts } from "../constants";
+import FileSaver from "file-saver";
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
-  const randomPrompt = surpriseMePrompt[randomIndex];
+  const randomPrompt = surpriseMePrompts[randomIndex];
 
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `doanload-${_id}.jpg`);
 }
